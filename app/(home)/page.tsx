96d235df949c87ac9131703f89346979278c6772
2Reis <2reis.developer@gmail.com>
2024-02-16 13:31:28 -0300

    feat: add search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 import Header from "../_components/header";
+import Search from "./components/search";
 
 export default function Home() {
   return (
@@ -16,6 +17,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
